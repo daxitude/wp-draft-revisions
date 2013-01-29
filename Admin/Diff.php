d0f3473ddca54c386c911b95cb5a051042ca040e
Dax Ponce de Leon <daxitude@gmail.com>
2013-01-29 12:30:32 -0600

    first pass at comparing attachment changes

--- Admin/Diff.php
+++ Admin/Diff.php
@@ -4,8 +4,10 @@
  */
 class DPR_Admin_Diff {
 	
+	// the left and right sides of the comparison (each a Post)
 	private $left;
 	private $right;
+	// fields to add to the comparison
 	private $fields_to_add = array(
 		'menu_order' => 'Menu Order',
 		'comment_status' => 'Comment Status',
@@ -13,7 +15,12 @@ class DPR_Admin_Diff {
 		'post_password' => 'Post Password',
 		'post_name' => 'Post Name'
 	);
+	
+	// meta keys not to include in the comparison
 	private $dont_include_meta = array( '_edit_last', '_edit_lock' );
+
+	// store the parents attachments
+	private $parents_attachments = false;
 	
 	// left is always the parent
 	public function __construct(&$left, &$right) {
@@ -24,11 +31,14 @@ class DPR_Admin_Diff {
 		$this->setup_post_meta($this->right);
 		$this->setup_post_taxonomies($this->left);
 		$this->setup_post_taxonomies($this->right);
+		$this->setup_post_attachments($this->left, true);
+		$this->setup_post_attachments($this->right);
 		
 		add_filter('_wp_post_revision_fields', array($this, 'add_rev_fields'));
 	}
 	
 	// setup post meta to be diffed
+
 	private function setup_post_meta($post) {
 		$meta = get_post_custom($post->ID);
 		
@@ -49,6 +59,33 @@ class DPR_Admin_Diff {
 			$term_names = array_map(create_function('$term', 'return $term->name;'), $terms);
 			$post->$tax = join(', ', $term_names);
 			$this->fields_to_add[$tax] = $tax;
+		}
+	}
+	
+	// attachments don't copy from parent to draft and remain when a draft is published,
+	// so for comparisons we'll symbolically show the parent attachments on the draft post
+	private function setup_post_attachments($post, $parent = false) {
+		$attachments = get_posts(array(
+			'post_type' => 'attachment',
+			'posts_per_page' => -1,
+			'post_parent' => $post->ID,
+		));
+				
+		if ( $parent )
+			$this->parents_attachments = $attachments;
+		else if( $this->parents_attachments )
+			$attachments = array_merge($attachments, $this->parents_attachments);
+
+		if (!$attachments) return;
+
+		$key = 'Attachments';
+		$this->fields_to_add[$key] = $key;
+		$post->key = '';
+		
+		foreach ( $attachments as $attachment ) {
+			$src = wp_get_attachment_image_src( $attachment->ID, 'thumbnail', true );
+			$edit_link = get_edit_post_link($attachment->ID);
+			$post->$key .= $src[0] . "\n";
 		}
 	}
 	
